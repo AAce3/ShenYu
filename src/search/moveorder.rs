a377dd170c2f20b1ae76b9650621b23b9f235b6c
Aaron Li <aaronhli617@gmail.com>
2022-08-29 14:09:33 -0400

    Completed staged movegen w/SEE refinement per HGM

--- src/search/moveorder.rs
+++ src/search/moveorder.rs
@@ -2,13 +2,13 @@ use crate::{
     board_state::{
         bitboard::{Bitboard, BB},
         board::Board,
-        typedefs::{BISHOP, KING, KNIGHT, NOPIECE, PAWN, QUEEN, ROOK},
+        typedefs::{Square, BISHOP, KING, KNIGHT, NOPIECE, PAWN, QUEEN, ROOK},
     },
     move_generation::{
         action::{Action, Move, ShortMove},
-        magic::{bishop_attacks, rook_attacks},
         makemove::{C1, C8, CASTLE, G1, G8, PASSANT},
-        masks::{KING_ATTACKS, KNIGHT_ATTACKS, PAWN_CAPTURES},
+        masks::{KING_ATTACKS, PAWN_CAPTURES},
+        movegen::INBETWEENS,
         movelist::{MoveList, ScoreList},
     },
 };
@@ -54,6 +54,12 @@ pub struct MovePicker<'a> {
     generated_killer_2: ShortMove,
 }
 
+impl MovePicker<'_>{
+    pub fn new<'a>(board: &'a Board, orderdata: &'a OrderData, ply: u8, ttmove: ShortMove) -> MovePicker<'a>{
+        MovePicker { board, orderdata, ply, curr_idx: 0, curr_scorelist: ScoreList::new(), curr_mvlist: MoveList::new(), curr_stage: Stage::TTMove, ttmove, generated_killer_1: 0, generated_killer_2: 0 }
+    }
+}
+
 impl Iterator for MovePicker<'_> {
     type Item = Move;
 
@@ -65,27 +71,37 @@ impl Iterator for MovePicker<'_> {
                 return Some(self.ttmove.to_longmove(self.board));
             }
             Stage::WCaptures => {
-                if self.curr_mvlist.length == 0 {
-                    self.curr_mvlist = self.board.generate_moves(false, true);
-                    for i in 0..self.curr_mvlist.length {
-                        let action = self.curr_mvlist[i as usize];
-                        self.curr_scorelist[i as usize] = self.board.capture_order(action);
-                    }
-                }
+                const WORST_GOOD_SCORE: i16 = 0;
                 let mut maxscore = i16::MIN;
                 let mut bestidx = self.curr_idx;
                 let mut second_best_score = i16::MIN;
-                // find next
-                for i in self.curr_idx..(self.curr_scorelist.length as usize) {
-                    if self.curr_scorelist[i] > maxscore {
-                        second_best_score = maxscore;
-                        maxscore = self.curr_scorelist[i];
-                        bestidx = i;
-                    } else if self.curr_scorelist[i] > second_best_score {
-                        second_best_score = self.curr_scorelist[i]; // we want to store the second best score as well
+                if self.curr_mvlist.length == 0 {
+                    self.curr_mvlist = self.board.generate_moves(false, true);
+
+                    for i in 0..(self.curr_mvlist.length as usize) {
+                        let action = self.curr_mvlist[i as usize];
+                        let value = self.board.capture_order(action);
+                        self.curr_scorelist[i as usize] = value;
+                        if value > maxscore {
+                            second_best_score = maxscore;
+                            maxscore = value;
+                            bestidx = i;
+                        } else if value > second_best_score {
+                            second_best_score = value;
+                        }
+                    }
+                } else {
+                    for i in self.curr_idx..(self.curr_scorelist.length as usize) {
+                        if self.curr_scorelist[i] > maxscore {
+                            second_best_score = maxscore;
+                            maxscore = self.curr_scorelist[i];
+                            bestidx = i;
+                        } else if self.curr_scorelist[i] > second_best_score {
+                            second_best_score = self.curr_scorelist[i]; // we want to store the second best score as well
+                        }
                     }
                 }
-                const WORST_GOOD_SCORE: i16 = 0;
+
                 self.curr_scorelist.swap(self.curr_idx, bestidx);
                 self.curr_mvlist.swap(self.curr_idx, bestidx);
                 let bestmove = self.curr_mvlist[self.curr_idx];
@@ -110,10 +126,18 @@ impl Iterator for MovePicker<'_> {
                 // test k1 and k2
                 if self.generated_killer_1 == 0 {
                     self.generated_killer_1 = killers[0];
-                    // check for legality of killer
+                    if self.board.check_legal(self.generated_killer_1) {
+                        return Some(self.generated_killer_1.to_longmove(self.board));
+                    } else {
+                        return self.next();
+                    }
                 } else if self.generated_killer_2 == 0 {
                     self.generated_killer_2 = killers[1];
-                    // verify killers
+                    if self.board.check_legal(self.generated_killer_2) {
+                        return Some(self.generated_killer_1.to_longmove(self.board));
+                    } else {
+                        return self.next();
+                    }
                 } else {
                     self.curr_stage = Stage::LCaptures;
                 }
@@ -131,27 +155,37 @@ impl Iterator for MovePicker<'_> {
 
                 self.curr_scorelist.swap(self.curr_idx, bestidx);
                 self.curr_mvlist.swap(self.curr_idx, bestidx);
+
                 let bestmove = self.curr_mvlist[self.curr_idx];
-                self.curr_idx += 1;
-                if bestmove as u16 == self.ttmove
-                    || bestmove as u16 == self.generated_killer_1
-                    || bestmove as u16 == self.generated_killer_2
-                {
+                if bestmove as u16 == self.ttmove {
                     return self.next();
+                } else {
+                    let seescore = self.board.see(bestmove);
+                    self.curr_scorelist[self.curr_idx as usize] = seescore;
+                    if seescore < maxscore {
+                        return self.next();
+                    }
+
+                    self.curr_idx += 1;
+                    if self.curr_idx as u8 == self.curr_mvlist.length {
+                        self.curr_mvlist = MoveList::new();
+                        self.curr_scorelist = ScoreList::new();
+                        self.curr_idx = 0;
+                        self.curr_stage = Stage::Quiets;
+                    }
+                    return Some(bestmove);
                 }
-                self.curr_idx += 1;
-                if self.curr_idx as u8 == self.curr_mvlist.length {
-                    self.curr_mvlist = MoveList::new();
-                    self.curr_scorelist = ScoreList::new();
-                    self.curr_idx = 0;
-                    self.curr_stage = Stage::Quiets;
-                }
-                return Some(bestmove);
             }
             Stage::Quiets => {
+                if self.curr_scorelist.length as usize == self.curr_idx {
+                    return None;
+                }
+                let mut maxscore = i16::MIN;
+                let mut bestidx = self.curr_idx;
+
                 if self.curr_mvlist.length == 0 {
                     self.curr_mvlist = self.board.generate_moves(true, false);
-                    for i in 0..self.curr_mvlist.length {
+                    for i in 0..(self.curr_mvlist.length as usize) {
                         let action = self.curr_mvlist[i as usize];
                         // assign history score
                         let piecemoved = action.piece_moved(self.board);
@@ -159,21 +193,20 @@ impl Iterator for MovePicker<'_> {
                         let history = self.orderdata.history[self.board.tomove as usize]
                             [piecemoved as usize][squareto as usize];
                         self.curr_scorelist[i as usize] = history;
+                        if history > maxscore {
+                            maxscore = history;
+                            bestidx = i;
+                        }
+                    }
+                } else {
+                    // resume prev movelist
+                    for i in self.curr_idx..(self.curr_scorelist.length as usize) {
+                        if self.curr_scorelist[i] > maxscore {
+                            maxscore = self.curr_scorelist[i];
+                            bestidx = i;
+                        }
                     }
                 }
-                if self.curr_scorelist.length as usize == self.curr_idx {
-                    return None;
-                }
-                let mut maxscore = i16::MIN;
-                let mut bestidx = self.curr_idx;
-                // resume prev movelist
-                for i in self.curr_idx..(self.curr_scorelist.length as usize) {
-                    if self.curr_scorelist[i] > maxscore {
-                        maxscore = self.curr_scorelist[i];
-                        bestidx = i;
-                    }
-                }
-
                 self.curr_scorelist.swap(self.curr_idx, bestidx);
                 self.curr_mvlist.swap(self.curr_idx, bestidx);
                 let bestmove = self.curr_mvlist[self.curr_idx];
@@ -192,11 +225,47 @@ impl Iterator for MovePicker<'_> {
 }
 
 impl Board {
-    // checks for pseudolegal move
-    fn check_legality(&self, action: ShortMove) -> bool {
+    pub fn check_legal(&self, action: ShortMove) -> bool {
+        if self.check_pseudolegal(action) {
+            let movingpiece = action.piece_moved(self);
+            if movingpiece == KING {
+                return true;
+            } else if action.move_type() == PASSANT {
+                let newb = self.do_move(action);
+                return newb.incheck(self.tomove);
+            } else {
+                let legalsquares = self.check_for_legalmoves();
+                let bpinsquares = self.generate_bishop_pins();
+                let rpinsquares = self.generate_rook_pins();
+
+                let tobb = Bitboard::new(action.move_to());
+                let frombb = Bitboard::new(action.move_from());
+                if tobb & legalsquares != 0 {
+                    if bpinsquares & frombb != 0 {
+                        return tobb & bpinsquares != 0
+                            && (movingpiece == BISHOP
+                                || movingpiece == QUEEN
+                                || movingpiece == PAWN);
+                    } else if rpinsquares & frombb != 0 {
+                        return tobb & rpinsquares != 0
+                            && (movingpiece == ROOK
+                                || movingpiece == QUEEN
+                                || movingpiece == PAWN);
+                    }
+                }
+            }
+        }
+        false
+    }
+
+    pub fn check_pseudolegal(&self, action: ShortMove) -> bool {
+        if action == 0 {
+            return false;
+        }
         let piecemoved = self.get_at_square(action.move_from());
         let is_correct_color = self.is_color(action.move_from(), self.tomove);
-        if is_correct_color && piecemoved != NOPIECE {
+        let legal_destination = !self.is_color(action.move_to(), self.tomove);
+        if is_correct_color && piecemoved != NOPIECE && legal_destination {
             match piecemoved {
                 PAWN => {
                     if self.is_empty(action.move_to()) {
@@ -216,51 +285,51 @@ impl Board {
                         return captures & newbb != 0;
                     }
                 }
-                KNIGHT => {
-                    let bbmove = Bitboard::new(action.move_to()) & !self[self.tomove];
-                    return KNIGHT_ATTACKS[action.move_from() as usize] & bbmove != 0;
+                KNIGHT | BISHOP | ROOK | QUEEN => {
+                    return Self::attackable(
+                        action.move_from(),
+                        action.move_to(),
+                        self.get_occupancy(),
+                    );
                 }
-                BISHOP => {
-                    let bbmove = Bitboard::new(action.move_to()) & !self[self.tomove];
-                    return bishop_attacks(action.move_from(), self.get_occupancy()) & bbmove != 0;
-                }
-                ROOK => {
-                    let bbmove = Bitboard::new(action.move_to()) & !self[self.tomove];
-                    return rook_attacks(action.move_from(), self.get_occupancy()) & bbmove != 0;
-                }
-                QUEEN => {
-                    let bbmove = Bitboard::new(action.move_to()) & !self[self.tomove];
-                    let atks = rook_attacks(action.move_from(), self.get_occupancy())
-                        | bishop_attacks(action.move_from(), self.get_occupancy());
-                    return atks & bbmove != 0;
-                }
+
                 KING => {
-                    let bbmove = Bitboard::new(action.move_to()) & !self[self.tomove];
+                    let occ = self.get_occupancy() ^ self.get_pieces(KING, self.tomove);
+                    let atkmask = self.generate_atk_mask(!self.tomove, occ);
+
                     if action.move_type() == CASTLE {
-                        const BLOCK_OCCUPIED_KINGSIDE: [Bitboard; 2] = [0x60, 0x6000000000000000];
-                        const BLOCK_OCCUPIED_QUEENSIDE: [Bitboard; 2] = [0xe, 0xe00000000000000];
-                        const BLOCK_CHECKED_KINGSIDE: [Bitboard; 2] = [0x70, 0x7000000000000000];
-                        const BLOCK_CHECKED_QUEENSIDE: [Bitboard; 2] = [0x1c, 0x1c00000000000000];
-                        let (relevant_castleright) = match action.move_to() {
-                            G1 => self.castling_rights >> 3,
-                            C1 => (self.castling_rights >> 2) & 1,
-                            G8 => (self.castling_rights >> 1) & 1,
-                            C8 => (self.castling_rights) & 1,
+                        let (relevant_castleright, blockmask, checkmask) = match action.move_to() {
+                            G1 => (self.castling_rights >> 3, 0x60, 0x70),
+                            C1 => ((self.castling_rights >> 2) & 1, 0xe, 0x1c),
+                            G8 => (
+                                (self.castling_rights >> 1) & 1,
+                                0x6000000000000000,
+                                0x7000000000000000,
+                            ),
+                            C8 => (
+                                (self.castling_rights) & 1,
+                                0xe00000000000000,
+                                0x1c00000000000000,
+                            ),
                             _ => panic!("Bad castle"),
                         };
-                        
 
+                        return relevant_castleright != 0
+                            && blockmask & occ == 0
+                            && atkmask & checkmask == 0;
                     } else {
-                        let atks = KING_ATTACKS[action.move_from() as usize];
+                        let bbmove = Bitboard::new(action.move_to());
+                        let atks = KING_ATTACKS[action.move_from() as usize] & !atkmask;
                         return atks & bbmove != 0;
                     }
-
-                    todo!()
                 }
                 _ => return false,
             }
         }
         false
+    }
+    fn attackable(from: Square, to: Square, occupancy: Bitboard) -> bool {
+        INBETWEENS[from as usize][to as usize] & occupancy == 0
     }
 }
 
@@ -273,9 +342,9 @@ impl Board {
         let attackerval = MATERIAL_VALUES[attacker as usize];
         let victimval = MATERIAL_VALUES[victim as usize];
         if attackerval > victimval {
-            self.see(action)
+            attackerval - victimval
         } else {
-            attackerval - victimval / 2
+            attackerval - victimval / 8
         }
     }
 }
