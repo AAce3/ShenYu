c5fc884e3c44f1882349f2623f0fa326b324e430
Aaron Li <aaronhli617@gmail.com>
2022-08-28 22:03:09 -0400

    fixed compiler warning

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(incomplete_features)]
 #![feature(core_intrinsics)]
 #![feature(adt_const_params)]
-
+#![feature(is_some_with)]
 pub mod board_state;
 pub mod move_generation;
 pub mod testing;
