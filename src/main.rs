b3a9742c995e0916cd687c56255b15b889ada27a
Aaron Li <aaronhli617@gmail.com>
2022-08-29 17:48:40 -0400

    clear main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use shenyu::board_state::board::Board;
+use shenyu::{board_state::{board::Board, typedefs::KNIGHT}, move_generation::{action::{ShortMove, Action}, makemove::PROMOTION}};
 
 fn main() {
-    let newb = Board::parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
-    newb.divide_perft(6);
+
 }
 
