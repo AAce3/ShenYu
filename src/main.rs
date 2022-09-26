94b3cfeba16efcabd81204819dbc46d5c86652d8
Aaron Li <aaronhli617@gmail.com>
2022-09-26 16:00:26 -0400

    simplified main function

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
+use shenyu::{uci::gameloop};
 
 
-use std::{thread, sync::mpsc};
-
-use shenyu::{
-    board_state::{board::Board},
-    search::{
-        alphabeta::{SearchData, SearchControl}, timer::Timer,
-    }, uci::{Communicator, Control},
-};
 
 fn main() {
-
-
-    let newb =
-        Board::parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
-    let newdata = SearchData::new(Timer::new());
-
-    let mut control = SearchControl{
-        searchdata: newdata,
-        curr_ply: 0,
-        curr_board: newb,
-    };
-    let mut comm = Communicator{
-        comm: None,
-    };
-    let (tx, rx) = mpsc::channel::<Control>();
-    control.searchdata.message_recv = Some(rx);
-    println!("Shen Yu by Aaron Li");
-    comm.comm = Some(tx);
-    thread::spawn(move || {
-        control.parse_commands();
-    });
-    comm.parse_commands();
+    gameloop()
 }
