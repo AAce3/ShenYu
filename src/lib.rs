26c3b907342f4f6d309399bcbae1fd950be93eb6
Aaron Li <aaronhli617@gmail.com>
2022-09-26 16:00:14 -0400

    added feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(adt_const_params)]
 #![feature(is_some_with)]
 #![feature(int_roundings)]
+#![feature(exclusive_range_pattern)]
 pub mod board_state;
 pub mod move_generation;
 pub mod testing;
