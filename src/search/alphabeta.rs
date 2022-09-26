9cce4fc4b9980759e94852166bcb7b055eb93fe2
Aaron Li <aaronhli617@gmail.com>
2022-09-26 16:01:06 -0400

    fixed window & pv bugs

--- src/search/alphabeta.rs
+++ src/search/alphabeta.rs
@@ -1,5 +1,4 @@
 use std::{
-    cmp,
     fmt::Write,
     sync::mpsc::{Receiver, TryRecvError},
     time::Instant,
@@ -17,7 +16,7 @@ use crate::{
 use super::{
     moveorder::{MovePicker, OrderData},
     timer::Timer,
-    transposition::{TranspositionTable, BETA, EXACT},
+    transposition::{TranspositionTable, ALPHA, BETA, EXACT},
 };
 const CHECKMATE: i16 = 10_000;
 
@@ -35,8 +34,8 @@ impl SearchControl {
         let mut bestmove = 0;
         let mut pv = List::new();
         let mut depth = 0;
-        let mut alpha = -CHECKMATE;
-        let mut beta = CHECKMATE;
+        let alpha = -CHECKMATE;
+        let beta = CHECKMATE;
         loop {
             depth += 1;
             pv.clear();
@@ -52,24 +51,15 @@ impl SearchControl {
             if self.searchdata.timer.stopped {
                 break;
             }
-            bestmove = pv[0];
-            if score <= alpha || score >= beta {
-                beta = CHECKMATE;
-                alpha = -CHECKMATE;
-                depth -= 1;
-                continue;
-            }
-            
-            alpha = cmp::max(score - 35, -CHECKMATE);
-            beta = cmp::min(score + 35, CHECKMATE);
-            let mut scoretype = "score cp";
+
+            let mut scoretype = "cp";
             let mut reported_score = score;
             if mated_in(score) < 64 && mated_in(score) > -64 {
                 scoretype = "mate";
                 reported_score = mated_in(score).div_ceil(2)
             }
             let elapsed = global_time.elapsed().as_millis() as u64;
-            
+
             let nps = if elapsed == 0 {
                 0
             } else {
@@ -77,20 +67,22 @@ impl SearchControl {
             };
 
             println!(
-                "info depth {} {} {} nodes {} nps {} pv{}",
+                "info depth {} score {} {} nodes {} qnodes {} nps {} time {} pv{}",
                 depth,
                 scoretype,
                 reported_score,
                 self.searchdata.nodecount,
+                self.searchdata.qnodecount,
                 nps,
+                elapsed,
                 format_pv(&pv)
             );
-
-            if depth >= self.searchdata.timer.maxdepth || elapsed * 3 > self.searchdata.timer.time_alloted{
+            bestmove = pv[0];
+            if depth >= self.searchdata.timer.maxdepth
+                || elapsed * 3 > self.searchdata.timer.time_alloted
+            {
                 break;
             }
-
-           
         }
         println!("bestmove {}", bestmove.to_algebraic());
         self.searchdata.timer.refresh();
@@ -130,7 +122,7 @@ impl Board {
     #[allow(clippy::too_many_arguments)]
     pub fn negamax<const ISROOT: bool>(
         &self,
-        mut depth: u8,
+        depth: u8,
         mut alpha: i16,
         beta: i16,
         data: &mut SearchData,
@@ -141,10 +133,7 @@ impl Board {
         if self.is_draw() {
             return 0;
         }
-        let incheck = self.incheck(self.tomove);
-        if incheck {
-            depth += 1;
-        }
+
         data.nodecount += 1;
         if data.nodecount >= data.timer.max_nodes {
             data.timer.stopped = true;
@@ -170,7 +159,13 @@ impl Board {
         if depth == 0 {
             // If we are at zero depth, Q-search will allow us to evaluate a quiet position for accurate results
             data.nodecount -= 1;
-            return self.quiesce(alpha, beta, 0, data);
+            let qvalue = self.quiesce(alpha, beta, 0, data, pvline);
+            if data.timer.stopped {
+                return 0;
+            } else {
+                return qvalue;
+            }
+
         }
 
         let mut bestmove = 0;
@@ -181,12 +176,15 @@ impl Board {
             // set our 'best guess' to be the tt move
             bestmove = tt_data.bestmove;
             let score = tt_data.score;
+            let shoulduse = match tt_data.get_nodetype() {
+                EXACT => true,
+                ALPHA => score <= alpha,
+                BETA => score > beta,
+                _ => false,
+            };
+
             // if we can use the score, then return that.
-            if !ISROOT
-                && tt_data.get_depth() >= depth
-                && (tt_data.get_nodetype() == EXACT
-                    || (tt_data.get_nodetype() == BETA && score >= beta))
-            {
+            if ply as u8 - starting_ply >= 3 && tt_data.get_depth() >= depth && shoulduse {
                 if mated_in(score) < 64 && mated_in(score) > -64 {
                     if score.is_positive() {
                         return score - (ply as i16 - starting_ply as i16);
@@ -217,6 +215,8 @@ impl Board {
 
         let mut stored_move = false;
         let mut num_moves = 0;
+        let mut raised_alpha = false;
+        
         for action in moves_generated {
             // Initialize a child PV line
             let mut newpvline = List::new();
@@ -235,6 +235,9 @@ impl Board {
                     starting_ply,
                     &mut newpvline,
                 );
+                if data.timer.stopped {
+                    return 0;
+                }
             } else {
                 // Otherwise, search with a null window to maximize cutoffs
                 score = -newb.negamax::<false>(
@@ -246,7 +249,13 @@ impl Board {
                     starting_ply,
                     &mut newpvline,
                 );
-                if score > alpha && score < beta {
+                if data.timer.stopped {
+                    return 0;
+                }
+
+                if score >= alpha && score < beta {
+                    newpvline.clear();
+                    newpvline.push(action);
                     // If the score is within the bounds then we have to do a full window re-search to get the true score
                     score = -newb.negamax::<false>(
                         depth - 1,
@@ -259,14 +268,15 @@ impl Board {
                     );
                 }
             }
-            if data.timer.stopped {
-                return 0;
-            }
+
             if score > best_score {
                 best_score = score;
                 best_pvline = newpvline;
                 bestmove = action as u16;
-                alpha = cmp::max(alpha, score);
+                if score > alpha {
+                    raised_alpha = true;
+                    alpha = score;
+                }
                 if score >= beta {
                     // The score is too good, and will be avoided. Stop searching
                     unsafe {
@@ -278,7 +288,7 @@ impl Board {
                             BETA,
                         );
                     }
-                    if !action.is_capture() && num_moves == 0 {
+                    if !action.is_capture() {
                         data.ord.update_history(action, depth, self);
                         data.ord.update_killer(action, ply);
                     }
@@ -290,6 +300,7 @@ impl Board {
 
             num_moves += 1;
         }
+        let nodetype = if raised_alpha { EXACT } else { ALPHA };
         if !stored_move {
             unsafe {
                 tt_entry.as_mut().unwrap().store(
@@ -297,11 +308,12 @@ impl Board {
                     bestmove,
                     best_score,
                     depth,
-                    EXACT,
+                    nodetype,
                 );
             }
         }
         if num_moves == 0 {
+            let incheck = self.incheck(self.tomove);
             if incheck {
                 return mate_score(ply, starting_ply);
             } else {
@@ -315,7 +327,14 @@ impl Board {
         best_score
     }
 
-    pub fn quiesce(&self, mut alpha: i16, beta: i16, ply: u16, data: &mut SearchData) -> i16 {
+    pub fn quiesce(
+        &self,
+        mut alpha: i16,
+        beta: i16,
+        ply: u16,
+        data: &mut SearchData,
+        pv: &mut List<Move>,
+    ) -> i16 {
         data.qnodecount += 1;
         data.nodecount += 1;
         if data.nodecount >= data.timer.max_nodes {
@@ -333,6 +352,8 @@ impl Board {
         if data.timer.is_timed && checknodes {
             data.timer.stopped = data.timer.check_time() || data.timer.stopped;
         }
+
+        
         if data.timer.stopped {
             return 0;
         }
@@ -341,26 +362,32 @@ impl Board {
         if bestscore >= beta {
             return bestscore;
         }
+
         if bestscore > alpha {
             alpha = bestscore;
         }
 
         let captures = MovePicker::new_capturepicker(self, &data.ord);
+        let mut best_pv = List::new();
         for action in captures {
-            let seevalue = self.see(action);
-            if seevalue + 200 + bestscore < alpha {
-                continue;
-            }
+            let mut newpv = List::new();
+            newpv.push(action);
             let newb = self.do_move(action);
-            let score = -newb.quiesce(-beta, -alpha, ply + 1, data);
-            if score >= beta {
-                return score;
-            }
+            
+            let score = -newb.quiesce(-beta, -alpha, ply + 1, data, &mut newpv);
 
             if score > alpha {
-                alpha = score
+                alpha = score;
+                best_pv = newpv;
+                if score >= beta {
+                    break;
+                }
             }
         }
+        for i in 0..best_pv.length {
+            pv.push(best_pv[i as usize]);
+        }
+
         alpha
     }
 }
